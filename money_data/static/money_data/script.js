12f99bf98450ae61adac177758a5d9f9ee2e49df
carsonharrell2 <thecarsonharrell@gmail.com>
2022-07-30 15:34:54 -0700

    delete confirmation

--- money_data/static/money_data/script.js
+++ money_data/static/money_data/script.js
@@ -10,3 +10,15 @@ function hideForm (){
     formElement.style.display = "none";
     buttonElement.style.display = "block";
 }
+
+//"location.href='{% url 'money_data:delete_log' m.id %}'"
+
+function trashCanPopUp() {
+    blur = document.getElementById('main');
+    blur2 = document.getElementById('navbar');
+    blur.classList.toggle('active');
+    blur2.classList.toggle('active');
+    popup = document.getElementById('popup');
+    popup.classList.toggle('active');
+    console.log(sessionStorage.getItem('id'));
+}
