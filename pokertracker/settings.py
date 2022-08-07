43e917bc663a0db8ff804bf40fa1bee52a7ec476
carsonharrell2 <thecarsonharrell@gmail.com>
2022-08-07 12:10:59 -0700

    trailing comma

--- pokertracker/settings.py
+++ pokertracker/settings.py
@@ -161,7 +161,7 @@ if cwd == '/app' or cwd[:4] == '/tmp':
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     STATIC_ROOT = 'staticfiles'
     STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, 'static')
+        os.path.join(BASE_DIR, 'static'),
     )
 
 # security.W016
